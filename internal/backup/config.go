399fd819f9ce81a8baca34798e90f04cf59af063
arauin <amandus.rauin@rwth-aachen.de>
2025-08-30 18:09:12 +0000

    add shutdown backup

--- internal/backup/config.go
+++ internal/backup/config.go
@@ -9,24 +9,25 @@ import (
 
 // Config holds the application configuration.
 type Config struct {
-	TimeZone     *time.Location `env:"TIME_ZONE"                     envDefault:"UTC"`
-	S3Region     string         `env:"S3_REGION"                     envDefault:"us-east-1"`
-	S3PathPrefix string         `env:"S3_PATH_PREFIX"                envDefault:"backups"`
-	DBUser       string         `env:"DB_USER,required"`
-	DBPassword   string         `env:"DB_PASSWORD,required"`
-	S3Endpoint   string         `env:"S3_ENDPOINT"`
-	S3AccessID   string         `env:"S3_ACCESS_KEY_ID,required"`
-	DBName       string         `env:"DB_NAME,required"`
-	S3SecretKey  string         `env:"S3_SECRET_ACCESS_KEY,required"`
-	S3Bucket     string         `env:"S3_BUCKET,required"`
-	DBHost       string         `env:"DB_HOST,required"`
-	LogLevel     string         `env:"LOG_LEVEL"                     envDefault:"info"`
-	CronSchedule string         `env:"CRON_SCHEDULE"                 envDefault:"@daily"`
-	RestoreKey   string         `env:"RESTORE_KEY"`
-	DumpInfix    string         `env:"DUMP_INFIX"                    envDefault:""`
-	DumpSuffix   string         `env:"DUMP_SUFFIX"                   envDefault:".sql.gz"`
-	DBPort       int            `env:"DB_PORT"                       envDefault:"5432"`
-	S3SSL        bool           `env:"S3_SSL"                        envDefault:"true"`
+	TimeZone             *time.Location `env:"TIME_ZONE"                     envDefault:"UTC"`
+	S3Region             string         `env:"S3_REGION"                     envDefault:"us-east-1"`
+	S3PathPrefix         string         `env:"S3_PATH_PREFIX"                envDefault:"backups"`
+	DBUser               string         `env:"DB_USER,required"`
+	DBPassword           string         `env:"DB_PASSWORD,required"`
+	S3Endpoint           string         `env:"S3_ENDPOINT"`
+	S3AccessID           string         `env:"S3_ACCESS_KEY_ID,required"`
+	DBName               string         `env:"DB_NAME,required"`
+	S3SecretKey          string         `env:"S3_SECRET_ACCESS_KEY,required"`
+	S3Bucket             string         `env:"S3_BUCKET,required"`
+	DBHost               string         `env:"DB_HOST,required"`
+	LogLevel             string         `env:"LOG_LEVEL"                     envDefault:"info"`
+	CronSchedule         string         `env:"CRON_SCHEDULE"                 envDefault:"@daily"`
+	RestoreKey           string         `env:"RESTORE_KEY"`
+	DumpInfix            string         `env:"DUMP_INFIX"                    envDefault:""`
+	DumpSuffix           string         `env:"DUMP_SUFFIX"                   envDefault:".sql.gz"`
+	DBPort               int            `env:"DB_PORT"                       envDefault:"5432"`
+	S3SSL                bool           `env:"S3_SSL"                        envDefault:"true"`
+	BackupBeforeShutdown bool           `env:"BACKUP_BEFORE_SHUTDOWN"        envDefault:"false"`
 }
 
 // LoadConfig loads the configuration from environment variables.
